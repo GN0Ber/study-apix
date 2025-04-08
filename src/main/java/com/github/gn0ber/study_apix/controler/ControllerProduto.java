353718b91713e45bef2822bc190f31cfaae462e4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 22:37:10 -0300

    Criando metodos get

--- src/main/java/com/github/gn0ber/study_apix/controler/ControllerProduto.java
+++ src/main/java/com/github/gn0ber/study_apix/controler/ControllerProduto.java
@@ -1,9 +1,13 @@
 package com.github.gn0ber.study_apix.controler;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +25,7 @@ public class ControllerProduto {
     @Autowired
     private ProdutoService produtoService;
 
+    // Métodos POST
     @PostMapping
     public ResponseEntity<Produto> create(@RequestBody ProdutoRequestCreate dto) {
 
@@ -29,16 +34,29 @@ public class ControllerProduto {
         return ResponseEntity.status(201).body(produto);
     }
 
+    // Métodos PUT
     @PutMapping
     public ResponseEntity<String> update() {
         return ResponseEntity.status(200).body("Produto Atualizado");
     }
-
+    
+    // Métodos GET
     @GetMapping
-    public ResponseEntity<String> find() {
-        return ResponseEntity.status(200).body("Batata");
+    public ResponseEntity<List<Produto>> findAll() {
+        List<Produto> produto = produtoService.findAll();
+        return ResponseEntity.status(200).body(produto);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {
+        Optional<Produto> opt = produtoService.findById(id);
+        if (opt.isPresent()) {
+            return ResponseEntity.ok(opt.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+    // Métodos DELETE
     @DeleteMapping
     public ResponseEntity<Void> delete() {
         return ResponseEntity.status(204).build();
--- src/main/java/com/github/gn0ber/study_apix/service/ProdutoService.java
+++ src/main/java/com/github/gn0ber/study_apix/service/ProdutoService.java
@@ -1,5 +1,8 @@
 package com.github.gn0ber.study_apix.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,4 +24,11 @@ public class ProdutoService {
         return produtoRepository.save(produto);
     }
 
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
 }
